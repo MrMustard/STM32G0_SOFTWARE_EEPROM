0e5183fbddfdf84fa80bb3f795d6794e8a3f2641
MrMustard <catodocomun@gmail.com>
2023-02-24 19:42:45 -0600

    firts funtional program to store only 1 varaible

---
+++ FIRMWARE/emulacion de eeprom/serial software git/STM32G0_SOFTWARE_EEPROM-experimento/FIRMWARE/SOFTWARE EEPROM/FLASH_EEPROM/Core/Src/main.c
@@ -0,0 +1,310 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2023 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include "serial_uart.h"// ibncluimos la libreria de serial uart
+#include "eeprom_mamalona.h"// incluimos la libreria de eeprom mamalona
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+UART_HandleTypeDef huart1;
+
+/* USER CODE BEGIN PV */
+int dataA = 255;
+int dataB = 0;
+int dataC = 2;
+uint32_t hexPage;
+uint32_t readData;
+
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_USART1_UART_Init(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_USART1_UART_Init();
+  /* USER CODE BEGIN 2 */
+SerialUartSendString("Iniciando el programa eeprom mamalona\n");
+hexPage = getHexAddressPage(DATA_PAGE);            //Get our hex page // nos devuelve los dat
+SerialUartSendVarInt("hex: %d", hexPage);
+SerialUartSendString("\n");
+
+//writeThreeData(hexPage, dataA, dataB, dataC);      //Write the data in to memory
+
+writeData(hexPage,1000, 33);
+
+//writeData(hexPage,1, 1);
+/*
+writeData(hexPage,2, 2);
+
+writeData(hexPage,3, 3);
+writeData(hexPage,4, 4);
+writeData(hexPage,5, 5);
+writeData(hexPage,6, 6);
+writeData(hexPage,7, 7);
+writeData(hexPage,8, 8);
+writeData(hexPage,9, 9);*/
+
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+
+	  readData = retrieveDataFromAddress(hexPage + (DATA_SPACE * 1000));
+	  SerialUartSendVarInt("Dato 0: %d", readData);
+	  SerialUartSendString("\n");
+
+	  readData = retrieveDataFromAddress(hexPage + (DATA_SPACE * 1));
+
+	  SerialUartSendVarInt("Dato 1: %d", readData);
+	  SerialUartSendString("\n");
+	  readData = retrieveDataFromAddress(hexPage + (DATA_SPACE * 2));
+	  SerialUartSendVarInt("Dato 2: %d", readData);
+	  SerialUartSendString("\n");
+	  readData = retrieveDataFromAddress(hexPage + (DATA_SPACE * 3));
+	  SerialUartSendVarInt("Dato 3: %d", readData);
+	  SerialUartSendString("\n");
+
+	  readData = retrieveDataFromAddress(hexPage + (DATA_SPACE * 4));
+	  SerialUartSendVarInt("Dato 4: %d", readData);
+	  SerialUartSendString("\n");
+
+	  readData = retrieveDataFromAddress(hexPage + (DATA_SPACE * 5));
+	  SerialUartSendVarInt("Dato 5: %d", readData);
+	  SerialUartSendString("\n");
+
+	  readData = retrieveDataFromAddress(hexPage + (DATA_SPACE * 6));
+	  SerialUartSendVarInt("Dato 6: %d", readData);
+	  SerialUartSendString("\n");
+
+	  readData = retrieveDataFromAddress(hexPage + (DATA_SPACE * 7));
+	  SerialUartSendVarInt("Dato 7: %d", readData);
+	  SerialUartSendString("\n");
+
+	  readData = retrieveDataFromAddress(hexPage + (DATA_SPACE * 8));
+	  SerialUartSendVarInt("Dato 8: %d", readData);
+	  SerialUartSendString("\n");
+
+	  readData = retrieveDataFromAddress(hexPage + (DATA_SPACE * 9));
+	  SerialUartSendVarInt("Dato 9: %d", readData);
+	  SerialUartSendString("\n");
+
+
+	  HAL_Delay(1000);
+
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
+  HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1);
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSIDiv = RCC_HSI_DIV1;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief USART1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART1_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART1_Init 0 */
+
+  /* USER CODE END USART1_Init 0 */
+
+  /* USER CODE BEGIN USART1_Init 1 */
+
+  /* USER CODE END USART1_Init 1 */
+  huart1.Instance = USART1;
+  huart1.Init.BaudRate = 115200;
+  huart1.Init.WordLength = UART_WORDLENGTH_8B;
+  huart1.Init.StopBits = UART_STOPBITS_1;
+  huart1.Init.Parity = UART_PARITY_NONE;
+  huart1.Init.Mode = UART_MODE_TX_RX;
+  huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart1.Init.OverSampling = UART_OVERSAMPLING_16;
+  huart1.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
+  huart1.Init.ClockPrescaler = UART_PRESCALER_DIV1;
+  huart1.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
+  if (HAL_UART_Init(&huart1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_UARTEx_SetTxFifoThreshold(&huart1, UART_TXFIFO_THRESHOLD_1_8) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_UARTEx_SetRxFifoThreshold(&huart1, UART_RXFIFO_THRESHOLD_1_8) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_UARTEx_DisableFifoMode(&huart1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART1_Init 2 */
+
+  /* USER CODE END USART1_Init 2 */
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
---
+++ FIRMWARE/emulacion de eeprom/serial software git/STM32G0_SOFTWARE_EEPROM-experimento/FIRMWARE/SOFTWARE EEPROM/FLASH_EEPROM/eeprom_mamalona/eeprom_mamalona.h
@@ -0,0 +1,27 @@
+/*
+ * eeprom_mamalona.h
+ *
+ *  Created on: Feb 24, 2023
+ *      Author: pc
+ */
+
+#ifndef EEPROM_MAMALONA_H_
+#define EEPROM_MAMALONA_H_
+
+#include "main.h"
+#include "stm32g0xx_hal_flash.h"
+
+#define FLASH_INIT  0x08000000  //This is the page zero of our flash
+#define DATA_SPACE  8            //Samples between each saved element
+#define PAGE_SECTOR 2048         //Page size
+#define DATA_PAGE   31           //Page that will store our data
+
+uint32_t getHexAddressPage(int dataPage);                               //Here receive our page on int format and we get the address on hex format
+uint32_t retrieveDataFromAddress(uint32_t hexAddress);                  //Retrieve our stored data
+void writeThreeData(uint32_t hexPage, int dataA, int dataB, int dataC); //Write our desired values in to flash
+void memoryPageErase(uint32_t memoryPage);                              //Erase a memory page from the flash retrieve
+
+//funcion para escribir una varaible en el index
+void writeData(uint32_t page,uint32_t index, int Data);
+
+#endif /* EEPROM_MAMALONA_H_ */
